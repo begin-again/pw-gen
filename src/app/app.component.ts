e81eb00e749692ab995464305b8e20505dc4656c
Todd Hogarth <begin-again@users.noreply.github.com>
2020-03-01 12:36:54 -0500

    app works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw-gen';
+  password = "";
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  length = 0;
+
+  onButtonClick = (): void => {
+    this.password = this.generatePassword();
+  };
+
+  generatePassword = (): string => {
+    const numbers = `1234567890`;
+    const letters = `abcdefghijklmnopqrstuvwxyz`;
+    const symbols = `!@#$%^&*()`;
+
+    let validChars = "";
+    if(this.includeLetters) validChars += letters;
+    if(this.includeNumbers) validChars += numbers;
+    if(this.includeSymbols) validChars += symbols;
+
+    let pwd = '';
+    for(let i=0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      pwd += validChars[index];
+    }
+    return pwd;
+  };
+
+
+  onChangeLength = (pwdLength: string): void => {
+    const _number = parseInt(pwdLength);
+
+    this.length = isNaN(_number) ? 0 : _number;
+  };
+
+  onChangeChecks = (name: string): void => {
+    switch (name) {
+      case "letters":
+        this.includeLetters = !this.includeLetters;
+        break;
+      case "numbers":
+        this.includeNumbers = !this.includeNumbers;
+        break;
+      case "symbols":
+        this.includeSymbols = !this.includeSymbols;
+        break;
+    }
+  };
+
+
 }
